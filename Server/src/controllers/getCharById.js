013879ba450b220bdc5a09cacb3c375bacfce727
DiegoS <sdiegoss@gmail.com>
2023-05-25 18:17:26 +0100

    25demayo

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -2,30 +2,33 @@ const axios = require("axios");
 
 const URL = "https://rickandmortyapi.com/api/character";
 
-const getCharById = (req, res) => {
-  const { id } = req.params;
 
-  axios
-    .get(`${URL}/${id}`)
-    .then((response) => response.data)
-    .then(({ status, name, species, origin, image, gender }) => {
-      if (name) {
-        const character = {
-          id,
-          name,
-          species,
-          origin,
-          image,
-          gender,
-          status,
-        };
-        return res.status(200).json(character);
-      }
-      return res.status(404).send('Not found');
-    })
-    .catch(error => res.status(500).send(error.message))
+const getCharById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await axios.get(`${URL}/${id}`);
+    const { status, name, species, origin, image, gender } = response.data;
 
+    if (name) {
+      const character = {
+        id,
+        name,
+        species,
+        origin,
+        image,
+        gender,
+        status,
+      };
+      return res.status(200).json(character);
+    }
+
+    return res.status(404).send("Not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
+
+
 
 
 
@@ -35,3 +38,7 @@ const getCharById = (req, res) => {
 module.exports = {
   getCharById,
 };
+
+
+
+
