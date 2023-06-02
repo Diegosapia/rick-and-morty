72bb053a0d3bf3a948f5ff8ce8ed802357121c88
DiegoS <sdiegoss@gmail.com>
2023-06-02 15:37:53 +0100

    020623

--- Server/src/controllers/deleteFav.js
+++ Server/src/controllers/deleteFav.js
@@ -3,9 +3,11 @@ const {Favorite} = require('../DB_connection');
 const deleteFav = async (req, res)=> {
 try {
     const {id} = req.params
-    await Favorite.destroy({where : {id} })
+    const userDeleted = await Favorite.findByPk(id)
+    await userDeleted.destroy({where : {id} })
 
-return Favorite;
+    const allFavorites = await Favorite.findAll()
+return res.status(200).json(allFavorites)
 
 } catch (error) {
     return res.status(500).send({error: error.message})
--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -3,13 +3,13 @@ const { Favorite }= require('../DB_connection')
 
 const postFav = async (req, res)=> {
     try {
-        const {name, origin, status, image, species, gender} = req.body;
+        const {id ,name, origin, status, image, species, gender} = req.body;
         
 
-        if (!name || !origin || status || !image || !species || !gender ) {
+        if (!id || !name || !origin || status || !image || !species || !gender ) {
             return res.status(401).send("Faltan datos");
         }
-         await Favorite.findOrCreate({where: {name, origin, image, species, gender}})
+         await Favorite.findOrCreate({where: {id, name, origin, status, image, species, gender}})
     
         return Favorite.findAll()
 
