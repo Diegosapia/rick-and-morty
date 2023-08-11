06c7781e1ff859634c66c341479f0566b9e7a058
DiegoS <sdiegoss@gmail.com>
2023-08-11 15:15:41 +0100

    about

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -16,9 +16,9 @@ const About = () => {
       }, []);
     
     return (
-        <div className={style.conteinerAbout}>
-            <h1> Bienvenidos a mi About. </h1>
-            <img  className={style.imgAbout} src={cropimg} alt=''></img>
+      <div className={style.conteinerAbout}>
+            <h1>   </h1>
+          <img  className={style.imgAbout} src={cropimg} alt=''></img>
             <div className={style.parra}>
             <p className={style.p}> I am Diego Sapia, an Argentine from Buenos Aires, with a passion for the world of tech, technology, and engineering that has driven me since childhood. Throughout the years, I have dedicated myself to immersing in this field, working on real projects, and continuously expanding my knowledge. This journey has been incredibly rewarding and has led me to achieve significant personal and professional growth.
 
@@ -26,7 +26,14 @@ Having spent the last six years in England, I am now excited to take my professi
 
 I am deeply committed to continuous growth and self-improvement. Hence, I actively seek out chances to acquire new skills and stay updated with the latest advancements in the tech industry. My ultimate goal is to make a positive impact through my work, contributing to the development of cutting-edge solutions that benefit users worldwide.
 
-If you are interested in collaborating or considering me for any job opportunities, feel free to contact me at diegosapia@yahoo.com. I am open to new connections and excited about potential collaborations and professional engagements.</p>
+                    If you are interested in collaborating or considering me for any job opportunities, feel free to
+                    contact me at{' '}
+                    <a href="mailto:sdiegoss@gmail.com" target="_blank" rel="noopener noreferrer">
+                        sdiegoss@gmail.com
+                    </a>
+                    . I am open to new connections and excited about potential collaborations and professional
+                    engagements.
+                </p>
 
             </div>
         </div>
