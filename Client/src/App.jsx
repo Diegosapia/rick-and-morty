666f4d5fd034697285449980eadcc5a8c9064016
DiegoS <sdiegoss@gmail.com>
2024-03-02 19:21:23 +0100

    implementacion del middleware de login

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,11 +7,9 @@ import Detail from "./components/Detail/Detail";
 import Favorites from './components/Favorites/Favorites'
 import './App.css'
 import axios from "axios";
-import { useState } from "react";
-import {Route, Routes, useLocation, useNavigate } from "react-router-dom";
-
-
-
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
+const URL = 'http://localhost:3001/rickandmorty/login';
 
 
 function App() {
@@ -22,45 +20,45 @@ function App() {
   const [access, setAccess] = useState(false);
 
   const login = async (userData) => {
-    const { email, password } = userData;
-    const URL = 'http://localhost:3001/rickandmorty/login';
-    
     try {
-      const response = await axios.get(URL, { params: { email, password } });
-      const { access } = response.data;
+      const { email, password } = userData;
+      const { data } = await axios.get(URL, { params: { email, password } });
+      const { access } = data;
       setAccess(access);
-      if (access) {
-        navigate('/home');
-      }
+      if (!access) throw Error()
+      access && navigate('/home');
     } catch (error) {
-      !access && navigate("/");
       alert("The user or the password is incorrect");
     }
   };
+  useEffect(() => {
+    !access && navigate('/');
+  }, [access]);
 
 
-  const onSearch = async (id) =>{
+  const onSearch = async (id) => {
     try {
       const { data } = await axios.get(`http://localhost:3001/rickandmorty/character/${id}`)
       if (data.name) {
-          setCharacters((oldChars) => [...oldChars, data]); }
+        setCharacters((oldChars) => [...oldChars, data]);
+      }
     } catch (error) {
-       alert(error.response.data);
+      alert(error.response.data);
     }
   }
   const onClose = (id) => {
     setCharacters(characters.filter((char) => char.id !== id));
   };
-  
+
   return (
     <div className="App">
       {location.pathname !== "/" ? <Nav onSearch={onSearch} /> : null}
       <Routes>
         <Route path="/" element={<Form login={login} />} />
-        <Route path="/home"element={<Cards characters={characters} onClose={onClose} />} />
+        <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
         <Route path="/favorites" onClose={onClose} element={<Favorites />} />
         <Route path="/about" element={<About />} />
-        <Route path="/detail/:id" element={<Detail  characters={characters}/>} />
+        <Route path="/detail/:id" element={<Detail characters={characters} />} />
       </Routes>
     </div>
   );
