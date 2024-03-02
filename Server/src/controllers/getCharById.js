ba330345d3a591084f3445d18ff4ef508b920144
DiegoS <sdiegoss@gmail.com>
2024-03-02 14:36:10 +0100

    actualizacion y mejora de los controllers

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,11 +1,12 @@
 const axios = require("axios");
 
 const URL = "https://rickandmortyapi.com/api/character";
+
+
 const getCharById = async (req, res) => {
   try {
     const { id } = req.params;
     const response = await axios.get(`${URL}/${id}`);
-    console.log("response",response)
     const { status, name, species, origin, image, gender } = response.data;
     if (name) {
       const character = {
@@ -17,25 +18,15 @@ const getCharById = async (req, res) => {
         gender,
         status,
       };
+      
       return res.status(200).json(character);
     }
-    return res.status(404).send("Not found");
+    return res.status(404).send("Not Found");
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send("Character not exist");
   }
 };
-
-
-
-
-
-
-
 
 module.exports = {
   getCharById,
 };
-
-
-
-
--- Server/src/controllers/postFav.js
+++ Server/src/controllers/postFav.js
@@ -4,12 +4,20 @@ const { Favorite }= require('../DB_connection')
 const postFav = async (req, res)=> {
     try {
         const {name, origin, status, image, species, gender} = req.body;
-        
-
         if ( !name || !origin || !status || !image || !species || !gender ) {
             return res.status(401).send("Faltan datos");
         }
-         await Favorite.findOrCreate({where: { name, origin, status, image, species, gender}})
+        const favoriteData = {
+            name,
+            origin,
+            status,
+            image,
+            species,
+            gender,
+            isfav :true,
+            
+        };
+         await Favorite.findOrCreate({where:  favoriteData})
     
          const allFavorites = await Favorite.findAll()
         return res.status(200).json(allFavorites)
